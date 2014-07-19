a1fde3e26d06b7d89b3d4f492b3841dae56f5d58
Zixxy <Sylwekklocek@gmail.com>
2014-07-19 02:25:33 +0200

    Added simple view with drawing components list.

--- Graphen/MainWindow.xaml.cs
+++ Graphen/MainWindow.xaml.cs
@@ -10,7 +10,6 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Graphen
@@ -23,6 +22,14 @@ namespace Graphen
         public MainWindow()
         {
             InitializeComponent();
+            this.Hide();
+            MenuWindow menu = new MenuWindow();
+            menu.Show();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
---
+++ Graphen/Menu.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Graphen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MenuWindow : Window
+    {
+        public MenuWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenNewSolution(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            Application.Current.MainWindow.Show();
+        }
+
+        //TODO
+        private void LoadPreviousGraph(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        //TODO
+        private void OpenSettingsDialog(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ExitApp(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            Application.Current.MainWindow.Close();
+        }
+    }
+}
