3c9964fd175d6e94a0d18eca012b58a366dec33f
Zixxy <Sylwekklocek@gmail.com>
2014-07-29 00:49:48 +0200

    Changed architectual pattern. Added simple controler.

---
+++ Graphen/ViewModel/Circle.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+using System.Windows.Media;
+using System.Windows;
+
+namespace Graphen.ViewModel
+{
+    public class Circle
+    {
+        public readonly Ellipse ellipse;
+        private System.Windows.Point position;
+
+        public Circle(System.Windows.Point position)
+        {
+            ellipse = new Ellipse(){
+                 Width = 20,
+                 Height = 20,
+                 Fill = new SolidColorBrush() 
+                 { 
+                     Color = Color.FromArgb(255, 0, 150, 0) 
+                 },
+                 Margin = new Thickness(position.X - 10, position.Y - 10, 0, 0),
+            };
+            this.position = position;
+        }
+
+        public System.Windows.Point Position
+        {
+            get
+            {
+                return position;
+            }
+            set
+            {
+                ellipse.Margin = new Thickness(value.X - 10, value.Y - 10, 0, 0);
+                position = value;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return ellipse.GetHashCode();
+        }
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Circle))
+                return false;
+            Circle o = (Circle)obj;
+            return o.ellipse.Equals(ellipse)  && Position == o.Position;
+        }
+
+        public static bool operator ==(Circle a, Circle b)
+        {
+            if (Object.ReferenceEquals(a, null) && Object.ReferenceEquals(b, null))
+                return true;
+            else if (Object.ReferenceEquals(a, null))
+                return false;
+            else
+                return a.Equals(b);
+        }
+
+        public static bool operator !=(Circle a, Circle b)
+        {
+            return !(a == b);
+        }
+        public static bool DetectColision(Circle a, Circle b)
+        {
+            var r1 = a.ellipse.Width/2;
+            var r2 = b.ellipse.Width/2;
+            double d = Math.Sqrt((a.Position.X - b.Position.X) * (a.Position.X - b.Position.X) + 
+                (a.Position.Y - b.Position.Y) * (a.Position.Y - b.Position.Y));
+            return d < r1 + r2;
+        }
+    }
+}
---
+++ Graphen/ViewModel/Controler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Graphen.Graph;
+
+namespace Graphen.ViewModel
+{
+    // this class probobly should be singleton. Idk if this should be named controler? xD
+    public class Controler : INotifyPropertyChanged
+    {
+        private Dictionary<Circle, Vertex> vertices; // we may do it using hashmap, previously implementing good hash function to circle.
+        private Dictionary<Edge, System.Windows.Shapes.Line> edges;
+        public static readonly Controler controler = new Controler();
+        private Controler()
+        {
+            vertices = new Dictionary<Circle,Vertex>();
+            edges = new Dictionary<Edge, System.Windows.Shapes.Line>();
+        }
+        event PropertyChangedEventHandler INotifyPropertyChanged.PropertyChanged
+        {
+            add {  }
+            remove { }
+        }
+
+        /*{Binding PickCircleToolProp}*/
+
+        public void AddVertex(Circle circle)
+        {
+            vertices.Add(circle, new Vertex());
+        }
+
+        public void AddEdge(System.Windows.Shapes.Line line, Circle o1, Circle o2)
+        {
+            Vertex v;
+            Vertex w;
+            vertices.TryGetValue(o1, out v);
+            vertices.TryGetValue(o1, out w);
+            Edge e = new Edge(v, w);
+            if(!edges.ContainsKey(e))
+                edges.Add(e, line);
+        }
+    }
+}
